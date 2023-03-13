6d42f9406e0b68833c3f0192982d976650838647
Caio Quirino <caioquirino@gmail.com>
2023-03-13 20:34:29 +0100

    Initial exec command

--- packages/cli/src/lib/cli.ts
+++ packages/cli/src/lib/cli.ts
@@ -1,11 +1,31 @@
 import { Command } from 'commander'
-import {version} from '../../package.json';
+import {version} from '../../package.json'
+import {spawn} from 'child_process'
 
-const program = new Command();
-program.description('@awslu/cli');
-program.version(version || "Unknown");
+const program = new Command()
+
+const execCommand = program.command("exec")
+execCommand.allowExcessArguments(true)
+  .option("-p, --profile <profile>", "AWS profile to use")
+  .option("-r, --region <region>", "AWS region to use")
+  .arguments("<cmd>")
+  .action(async (command, options, commandObj) => {
+    const child = spawn(commandObj.args[0], commandObj.args.slice(1), {
+      stdio: [process.stdin, process.stdout, process.stderr],
+      detached: false,
+      shell: true
+    })
+    child.on('close', (code) => {
+      if(code > 0) {
+        process.exit(code)
+      }
+    });
+  })
+
+program.description('@awslu/cli')
+program.version(version || "Unknown")
 
 export const cli = async (): Promise<void> => {
 
-  await program.parseAsync();
+  await program.parseAsync()
 }
