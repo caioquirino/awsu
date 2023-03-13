ab0d5f7d24df26f9e904590cf188bfbe22836411
Caio Quirino <caioquirino@gmail.com>
2023-03-13 19:49:22 +0100

    Initial commander args

--- packages/cli/src/lib/cli.ts
+++ packages/cli/src/lib/cli.ts
@@ -1,3 +1,11 @@
-export function cli(): void {
-  console.log('@awsu/cli')
+import { Command } from 'commander'
+import {version} from '../../package.json';
+
+const program = new Command();
+program.description('@awslu/cli');
+program.version(version || "Unknown");
+
+export const cli = async (): Promise<void> => {
+
+  await program.parseAsync();
 }
